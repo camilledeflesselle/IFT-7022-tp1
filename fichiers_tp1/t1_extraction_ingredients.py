648b00b440c0d5b0f5d11231582461df2197ce83
deflessc <camille.deflesselle@edu.esiee.fr>
2022-09-22 20:55:14 -0400

    Add : Ex1 - 12,5% d'erreur

--- fichiers_tp1/t1_extraction_ingredients.py
+++ fichiers_tp1/t1_extraction_ingredients.py
@@ -2,12 +2,19 @@
 import re
 
 ingredients_fn = "./data/ingredients.txt"
+solutions_fn =  "./data/ingredients_solutions.txt"
 
 # Mettre dans cette partie la (les) expression(s) régulière(s)
 # que vous utilisez pour analyser les ingrédients
 #
 # Vos regex ici...
 #
+pattern_digit = "[\d]+((,|\/)[\d]+)?"
+pattern_unit_abrev = "m|g|k|po|lb|l|L|oz"
+pattern_unit_spoon = "(c\.|cuillère)s?\sà\s[A-zÀ-ü]+\.?"
+pattern_unit_words = "(rôti\sde|tasse|pincée|gousses|Bouquet|Rondelle|enveloppe|tranche)s?"
+pattern_quantity = "(\(?" + pattern_digit + "\s" +"(" +pattern_unit_spoon + "|" + pattern_unit_words + "|" + pattern_unit_abrev +")" + "(\.|é|\b)\)?)"
+pattern_quantity = r"((\s\()?[\d]+((,|\/)[\d]+)?(\s(c\.|cuillère)s?\sà\s\.?[A-zÀ-ÿ-é]+\.?|\s((rôti\sde|tasse|Bouquet|Rondelle|pincée|gousse|enveloppe|tranche)s?|m|g|k|po|lb|l|L|oz)+\b)?\)?)+"
 
 def load_ingredients(filename):
     with open(filename, 'r', encoding='utf-8') as f:
@@ -15,6 +22,15 @@ def load_ingredients(filename):
     ingredients = [x.strip() for x in raw_items]
     return ingredients
 
+def get_solutions(filename, colnames = ["ITEM", "QUANTITY", "INGREDIENT"]):
+    solutions = load_ingredients(solutions_fn)
+    quantities = []
+    ingredients = []
+    for solution in solutions :
+        solution = solution.split(sep="   ")
+        quantities.append(re.sub(r'QUANTITE:', "", solution[1]))
+        ingredients.append(re.sub(r'INGREDIENT:', "", solution[2]))
+    return ingredients, quantities
 
 def get_ingredients(text):
     # Insérez ici votre code pour l'extraction d'ingrédients.
@@ -27,7 +43,19 @@ def get_ingredients(text):
     #
     # Votre code ici...
     #
-    return "2 cuillères à café", "poudre à pâte"   # À modifier - retourner la paire extraite
+    x = re.search(pattern_quantity, text)
+    if x :
+        ingredient = re.sub(pattern_quantity, "", text)
+        # quantity = ' '.join(x.groups())
+        quantity = x.group(0)
+
+    else:
+        ingredient = text
+        quantity = ""
+    return quantity, ingredient
+
+
+
 
 
 
@@ -35,9 +63,14 @@ if __name__ == '__main__':
     # Vous pouvez modifier cette section
     print("Lecture des ingrédients du fichier {}. Voici quelques exemples: ".format(ingredients_fn))
     all_items = load_ingredients(ingredients_fn)
-    for item in all_items[:5]:
-        print("\t", item)
-    print("\nExemples d'extraction")
-    for item in all_items[:5]:
-        quantity, ingredient = get_ingredients(item)
-        print("\t{}\t QUANTITE: {}\t INGREDIENT: {}".format(item, quantity, ingredient))
+    ingredients, quantities = get_solutions(solutions_fn)
+    print("\nExtractions fausses :")
+    print("Result / Truth")
+    errors = []
+    for i in range(len(ingredients)):
+        quantity, ingredient = get_ingredients(all_items[i])
+        result = "\t{}\t QUANTITE: {}\t INGREDIENT: {}".format(item, quantity, ingredient)
+        if quantity != quantities[i] : 
+            errors.append(result)
+            print(quantity, "/", quantities[i])
+    print("Percentage of errors : {}%".format(len(errors)/len(solutions)*100))
